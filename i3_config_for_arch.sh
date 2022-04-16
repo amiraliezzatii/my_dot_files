e16ce88e586c5982cd1f085c5f24c8e1fb8a0e97
amit | امیر <80686133+amiraliezzatii@users.noreply.github.com>
2022-04-16 13:57:29 -0400

    Add files via upload

---
+++ i3_config_for_arch.sh
@@ -0,0 +1,8 @@
+sudo pacman -S vim 
+sudo pacman -S i3
+sudo pacman -S git 
+mkdir gitfiles
+cd gitfiles
+git clone https://github.com/tobi-wan-kenobi/bumblebee-status.git
+sudo cp -r bumblebee-status ~/.config
+
