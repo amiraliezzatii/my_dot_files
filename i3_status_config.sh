4627b2fb9eb87cdedb9a6af422d530f78c60b4d3
amit | امیر <80686133+amiraliezzatii@users.noreply.github.com>
2022-04-16 13:40:13 -0400

    Add files via upload

---
+++ i3_status_config.sh
@@ -0,0 +1,8 @@
+sudo apt install vim 
+sudo apt install i3 
+sudo apt install git
+mkdir gitfiles
+cd gitfiles 
+git clone https://github.com/tobi-wan-kenobi/bumblebee-status.git
+sudo cp -r bumblebee-status ~/.config 
+
